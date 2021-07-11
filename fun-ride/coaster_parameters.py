9cfdc720a15bfe4d75d2110d6cac6e112aadad5f
wajidmanzoor <wajid.manzoor.1999@gmail.com>
2021-07-11 22:16:18 +0530

    coaster parameters added

---
+++ fun-ride/coaster_parameters.py
@@ -0,0 +1,14 @@
+intial_height = 0
+max_height =50
+min_height = 0
+intial_velocity = 30
+max_velocity = 5 #after drop
+intial_track_angle = 0
+track_lenght = 120
+friction_coef = 0.01
+drag_coef = 0.3
+max_gforce = 4
+min_gforce = 0
+plot_lift_hill = 0
+plot_mutations = 1
+
